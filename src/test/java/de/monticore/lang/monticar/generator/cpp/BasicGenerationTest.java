c567fa851408c8f39dff6c4f3f79b41cb939fde5
Sascha Schneiders <sascha.schneiders@rwth-aachen.de>
2017-12-17 16:07:10 +0100

    added additional MathUnit files

--- src/test/java/de/monticore/lang/monticar/generator/cpp/BasicGenerationTest.java
+++ src/test/java/de/monticore/lang/monticar/generator/cpp/BasicGenerationTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertNotNull;
 /**
  * @author Sascha Schneiders
  */
-public class BasicGenerationTest  extends AbstractSymtabTest {
+public class BasicGenerationTest extends AbstractSymtabTest {
 
     @Test
     public void testBasicLookUpInstanceGeneration() throws IOException {
@@ -32,7 +32,7 @@ public class BasicGenerationTest  extends AbstractSymtabTest {
     }
 
     @Test
-    public void testSampleModel() throws IOException{
+    public void testSampleModel() throws IOException {
         TaggingResolver symtab = createSymTabAndTaggingResolver("src/test/resources");
 
         ExpandedComponentInstanceSymbol componentSymbol = symtab.<ExpandedComponentInstanceSymbol>resolve("testing.model", ExpandedComponentInstanceSymbol.KIND).orElse(null);
@@ -40,5 +40,59 @@ public class BasicGenerationTest  extends AbstractSymtabTest {
         GeneratorCPP generatorCPP = new GeneratorCPP();
         generatorCPP.setGenerationTargetPath("./target/generated-sources-cpp/testing/l0");
         List<File> files = generatorCPP.generateFiles(componentSymbol, symtab);
+    }
+
+    @Test
+    public void testMathUnitAllLevelsInstancing() throws IOException {
+        for (int i = 1; i <= 8; ++i) {
+            testMathUnitInstancing(i);
+        }
+    }
+
+
+    private void testMathUnitInstancing(int number) throws IOException {
+        testMathUnitInstancing(number, false);
+        testMathUnitInstancing(number, true);
+    }
+
+    private void testMathUnitInstancing(int number, boolean useArmadillo) throws IOException {
+        testMathUnitInstancing(number, false, false, useArmadillo);
+        testMathUnitInstancing(number, true, false, useArmadillo);
+        testMathUnitInstancing(number, false, true, useArmadillo);
+        testMathUnitInstancing(number, true, true, useArmadillo);
+    }
+
+    private void testMathUnitInstancing(int number, boolean enableThreadingOptimization, boolean enableAlgebraicOptimizations, boolean useArmadillo) throws IOException {
+        TaggingResolver symtab = createSymTabAndTaggingResolver("src/test/resources");
+
+        ExpandedComponentInstanceSymbol componentSymbol = symtab.<ExpandedComponentInstanceSymbol>resolve("paper.mathUnit"+number, ExpandedComponentInstanceSymbol.KIND).orElse(null);
+        assertNotNull(componentSymbol);
+        GeneratorCPP generatorCPP = new GeneratorCPP();
+        String additionalPath = "";
+        if (enableAlgebraicOptimizations) {
+            generatorCPP.setUseAlgebraicOptimizations(true);
+            if (enableThreadingOptimization)
+                additionalPath = "3";
+            else
+                additionalPath = "1";
+        }
+        if (enableThreadingOptimization) {
+            generatorCPP.setUseThreadingOptimization(true);
+            if (enableAlgebraicOptimizations)
+                additionalPath = "3";
+            else
+                additionalPath = "2";
+        }
+        if (!enableAlgebraicOptimizations && !enableThreadingOptimization)
+            additionalPath = "0";
+        String firstPartOfPath = "";
+        if (useArmadillo) {
+            generatorCPP.useArmadilloBackend();
+            firstPartOfPath = "./target/generated-sources-cpp/armadillo/paperMatrixModifier/l";
+        } else {
+            firstPartOfPath = "./target/generated-sources-cpp/paperMatrixModifier/l";
+        }
+        generatorCPP.setGenerationTargetPath(firstPartOfPath + additionalPath);
+        generatorCPP.generateFiles(componentSymbol, symtab);
     }
 }
