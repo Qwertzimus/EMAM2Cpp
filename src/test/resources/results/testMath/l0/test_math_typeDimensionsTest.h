45c9750e037c833595303239d4bcd642824b1731
Christoph Richter <christoph.richter@rwth-aachen.de>
2018-04-12 18:05:20 +0200

    TypeConverter, ExecuteMethodGeneratorHandler: Added column vector support for dimension equals 1 (fixes #52)

---
+++ src/test/resources/results/testMath/l0/test_math_typeDimensionsTest.h
@@ -0,0 +1,21 @@
+#ifndef TEST_MATH_TYPEDIMENSIONSTEST
+#define TEST_MATH_TYPEDIMENSIONSTEST
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#include "armadillo.h"
+using namespace arma;
+class test_math_typeDimensionsTest{
+public:
+void init()
+{
+}
+void execute()
+{
+double value;
+colvec vec3=colvec(3);
+mat mat33=mat(3,3);
+}
+
+};
+#endif
